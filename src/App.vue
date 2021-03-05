6bf6c1f036f0147bba6b9a76126ee4410be96ba4
ryuzuka <ryuzuka@gmail.com>
2021-03-05 10:45:05 +0900

    moment: lang -> locale

--- src/App.vue
+++ src/App.vue
@@ -29,8 +29,7 @@ export default {
         })
     }
 
-    this.$moment.locale('ko')
-    this.$moment.lang('ko', {
+    this.$moment.locale('ko', {
       weekdays: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
       weekdaysShort: ['일', '월', '화', '수', '목', '금', '토']
     })
