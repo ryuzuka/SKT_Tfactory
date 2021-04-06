1ea88514b04e85511d765c12f17c4c02a5cac213
ryuzuka <ryuzuka@gmail.com>
2021-04-06 19:22:11 +0900

    constant rolllback

--- src/constant.js
+++ src/constant.js
@@ -14,35 +14,5 @@ export default {
   'COUNSELING_CODE': {},
   'ON_SITE_COUNSELING_DETAIL': '',
   'MDN': '',
-  'CERT_KEY': '',
-  'DATA': {
-    'RET_CODE': 0,
-    'PROGRAM_CLASS': [
-      {
-        'PROGRAM_CLASS_ID': 33,
-        'PROGRAM_SCHEDULE_ID': 82,
-        'BOOK_ID': 2180,
-        'BOOK_CODE': '20-4011',
-        'TYPE': 'program',
-        'STORE_ID': 23241,
-        'ZONE_ID': 44473,
-        'COUNSELING_ID': 33,
-        'DATE': '2021-03-20T05:51:00.000Z',
-        'STATUS': 'ordered',
-        'OPERATOR_ID': null,
-        'USER_ID': 23478,
-        'USER_NAME': '한민지',
-        'USER_UNMASKING_NAME': '한리경',
-        'USER_NICKNAME': null,
-        'USER_PHONE_NUMBER': '01057556895',
-        'USER_SERVICE_MGMT_NUMBER': '',
-        'MODIFIED_BOOK_ID': null,
-        'COUNSELING_START_YMDT': null,
-        'REG_YMDT': '2021-03-18T05:52:43.000Z',
-        'EDIT_YMDT': '2021-03-18T05:52:43.000Z',
-        'DEL_YN': 0,
-        'DEL_YMDT': null
-      }
-    ]
-  }
+  'CERT_KEY': ''
 }
