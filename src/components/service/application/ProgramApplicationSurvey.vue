574aeee9aa563639c5d08d18e3c93f41f9f4a889
ryuzuka <ryuzuka@gmail.com>
2021-04-09 17:40:08 +0900

    프로그램 신청 완료

--- src/components/service/application/ProgramApplicationSurvey.vue
+++ src/components/service/application/ProgramApplicationSurvey.vue
@@ -7,32 +7,40 @@ export default {
   name: 'ProgramApplicationSurvey',
   data () {
     return {
-      storeId: this.$route.query.store_id,
+      storeId: '',
       classId: this.$route.query.classId,
       questionList: [],
       contactNumber: '',
       attendeeNum: 1,
-      scheduleId: '',
-      bookId: ''
+      scheduleId: ''
     }
   },
   mounted () {
-    this.scheduleId = sessionStorage.getItem('scheduleId')
     this.contactNumber = sessionStorage.getItem('contactNumber')
     this.attendeeNum = parseInt(sessionStorage.getItem('attendeeNum'))
-    this.questionList = JSON.parse(sessionStorage.getItem('surveyQuestions'))
-    this.questionList.forEach(question => {
-      question.ANSWER = []
-    })
 
-    // STORE.getQuestions(this.$store.state.constants.COUNSELING.COUNSELING_ID).then(result => {
-    //   this.questionList = result.QUESTIONS
-    //   this.questionList.forEach(question => {
-    //     question.ANSWER = []
-    //   })
-    // })
+    this.getProgramClass()
   },
   methods: {
+    getProgramClass () {
+      STORE.getProgramClass(this.classId).then(result => {
+        this.programInfo = result.PROGRAM_CLASS
+
+        this.storeId = this.programInfo['STORE_ID']
+        this.questionList = this.programInfo['SURVEY_QUESTIONS']
+        if (this.questionList) {
+          this.questionList.forEach(question => {
+            question.ANSWER = []
+          })
+        }
+        this.checkProgramBook()
+      })
+    },
+    checkProgramBook () {
+      STORE.getProgramTimeTable(this.storeId, this.classId).then(result => {
+        this.scheduleId = result['SCHEDULE_LIST'][0].PROGRAM_SCHEDULE_ID
+      })
+    },
     canNotWriteOver200Error () {
       this.$modal.show('dialog', {
         title: this.$t('my.alert-booking-failed-200'),
@@ -48,21 +56,23 @@ export default {
       let BASIC_SURVEY_RESPONSE = []
       let over200Flag = false
 
-      this.questionList.forEach(question => {
-        if (JSON.stringify(question.ANSWER).length > 202) {
-          over200Flag = true
-          this.canNotWriteOver200Error()
-        }
+      if (this.questionList) {
+        this.questionList.forEach(question => {
+          if (JSON.stringify(question.ANSWER).length > 202) {
+            over200Flag = true
+            this.canNotWriteOver200Error()
+          }
 
-        if (question.ANSWER.length === 0) {
-          question.ANSWER = ''
-        }
-        let answer = {
-          'BASIC_SURVEY_QUESTION_ID': question.BASIC_SURVEY_QUESTION_ID,
-          'RESPONSE': JSON.stringify(question.ANSWER)
-        }
-        BASIC_SURVEY_RESPONSE.push(answer)
-      })
+          if (question.ANSWER.length === 0) {
+            question.ANSWER = ''
+          }
+          let answer = {
+            'BASIC_SURVEY_QUESTION_ID': question.BASIC_SURVEY_QUESTION_ID,
+            'RESPONSE': typeof (question.ANSWER) === 'object' ? JSON.stringify(question.ANSWER) : question.ANSWER
+          }
+          BASIC_SURVEY_RESPONSE.push(answer)
+        })
+      }
 
       if (!over200Flag) {
         this.$store.state.constants.COUNSELING.BASIC_SURVEY_RESPONSE = BASIC_SURVEY_RESPONSE
@@ -72,7 +82,7 @@ export default {
     applyProgram (surveyResponse) {
       // 신청
       let applyInfo = {
-        'STORE_ID': parseInt(this.storeId),
+        'STORE_ID': this.storeId,
         'USER_NAME': this.$cookies.get('MY_INFO').NAME,
         'PROGRAM_SCHEDULE_ID': this.scheduleId,
         'ATTENDEE_NUM': this.attendeeNum,
@@ -80,8 +90,7 @@ export default {
         'BASIC_SURVEY_RESPONSE': surveyResponse
       }
       STORE.applyProgram(applyInfo).then(result => {
-        this.bookId = result.BOOK_ID
-        this.$router.push('/sev/program/applicationComplete?&store_id=' + this.storeId + 'classId=' + this.$route.query.classId + '&bookId=' + this.bookId)
+        this.$router.push('/sev/program/applicationComplete?&bookId=' + result.BOOK_ID)
       })
     }
   }
