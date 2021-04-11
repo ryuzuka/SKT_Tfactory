917248216d30b704c7e31ccf73fd87f670e84d88
ryuzuka <ryuzuka@gmail.com>
2021-04-11 16:24:13 +0900

    예약/신청 내역 플래그 정리

--- src/components/mypage/booking/BookingList.vue
+++ src/components/mypage/booking/BookingList.vue
@@ -1,26 +1,34 @@
 <template src="../../../assets/html/mypage/booking/booking-list.html"></template>
 
 <script>
-/**
-   * success: 예약성공 ORDERED 상담요청
-   * cancel: 예약취소 CANCELED 취소
-   * complete: 완료 COMPLETED 상담완료
-   * end: 설문조사 등록 완료 SATISFACTION_SURVEYED 후기작성완료
-   * not: 미방문 NO_SHOW 미방문
-   * standby: 대기접수
-   */
 import * as STORE from '../../../js/store.js'
-import * as TID from '../../../js/tid.js'
-import * as USER from '../../../js/user.js'
+
+/**
+ * success: 예약성공 ORDERED 상담요청
+ * cancel: 예약취소 CANCELED 취소
+ * complete: 완료 COMPLETED 상담완료
+ * end: 설문조사 등록 완료 SATISFACTION_SURVEYED 후기작성완료
+ * not: 미방문 NO_SHOW 미방문
+ * standby: 대기접수
+ * apply: 예약 완료: 신청완료
+ * 대기
+ * 당첨
+ * 미당첨
+ */
 
 let BOOKING_STATUS = {
-  'ordered': 'success',
-  'operator_assigned': 'success',
-  'counseling': 'counseling',
-  'canceled': 'cancel',
-  'completed': 'complete',
-  'satisfaction_surveyed': 'end',
-  'no_show': 'not'
+  'ordered': 'success', // 예약 완료
+  'operator_assigned': 'success', // 예약 완료
+  'apply': 'apply', // 신청완료
+  'canceled': 'cancel', // 취소
+  'completed': 'complete', // 종료
+  'end': 'complete', // 종료
+  'counseling': 'counseling', // 상담중
+  'no_show': 'not', // 미방문
+  '대기': 'standby', // 대기
+  '당첨': '당첨', // 당첨
+  '미당첨': '미당첨', // 미당첨
+  'canceled_by_operator': 'cancel' // 취소
 }
 
 export default {
@@ -41,38 +49,18 @@ export default {
     },
     getBookingList () {
       STORE.getMyBookingList(this.lastIndex).then(result => {
-        result.BOOKING.forEach((booking) => {
+        result['BOOKING'].forEach((booking) => {
           booking.type = booking.TYPE
-          let STATUS = booking.STATUS
+          let STATUS = booking['STATUS']
           booking.status = BOOKING_STATUS[STATUS.toString()]
           booking.date = this.$moment(booking.DATE).format('YYYY.MM.DD HH:mm')
           this.lastIndex = booking.BOOK_ID
         })
-        this.listData = this.listData.concat(result.BOOKING)
+        this.listData = this.listData.concat(result['BOOKING'])
       })
     }
   },
   mounted () {
-    /** ******* T world 예약 관련 ******* */
-    /** ************** */
-    if (this.$route.query.sso_login_id) {
-      // localStorage.setItem('SERVICE_NAME', this.$route.query.service_name)
-      sessionStorage.setItem('SVC_NUM', this.$route.query.svc_num)
-      let callback = window.location.origin + window.location.pathname
-      TID.loginRedirectSso(this.$route.query.sso_login_id, callback)
-    }
-
-    if (this.$route.query.id_token) {
-      USER.signInOrUpTID(this.$route.query).then(result => {
-        if (result.RET_CODE === 0) {
-          this.$cookies.set('USER_AUTH', result)
-        } else {
-          this.$router.push({name: 'Error500'})
-        }
-      })
-    }
-    /** ************** */
-    /** ************** */
     if (!this.$cookies.get('USER_AUTH')) {
       localStorage.setItem('previous_url', '/my/booking/list')
       this.$router.push({name: 'Login'})
