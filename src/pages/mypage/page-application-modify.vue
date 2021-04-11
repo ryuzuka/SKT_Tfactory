26e370d3c4b5e11242a93e6ca12964fea41b3c83
ryuzuka <ryuzuka@gmail.com>
2021-04-11 18:30:20 +0900

    프로그램 신청 수정

---
+++ src/pages/mypage/page-application-modify.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrap">
+    <Header type="back" title="프로그램 신청"></Header>
+    <My-Application-Modify></My-Application-Modify>
+
+    <modals-container></modals-container>
+    <v-dialog></v-dialog>
+  </div>
+</template>
+
+<script>
+import Header from '../../components/common/Header'
+import MyApplicationModify from '../../components/mypage/application/ApplicationModify'
+
+export default {
+  name: 'page-my-program-application-modify',
+  components: {
+    Header,
+    MyApplicationModify
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+  }
+}
+</script>
