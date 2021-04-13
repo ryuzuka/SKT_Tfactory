a27a586714fc33f70e6c2b3ad404b8b13a5a79b1
ryuzuka <ryuzuka@gmail.com>
2021-04-14 01:45:21 +0900

    프로그램 상세 신청 / 예약

--- src/components/product/program/Program11.vue
+++ src/components/product/program/Program11.vue
@@ -2,33 +2,70 @@
 
 <script>
 import * as STORE from '../../../js/store.js'
+import _ from 'lodash'
 
 export default {
   name: 'Program11',
   data () {
     return {
       bookingAvailable: false,
-      isLogin: ''
+      programClass: null,
+      isApply: false,
+      classId: this.$route.query.classId,
+      storeId: '',
+      programInfo: {},
+      isLogin: false
     }
   },
   mounted () {
-    STORE.getProgramClass(this.$route.query.classId).then(result => {
-      this.bookingAvailable = result.PROGRAM_CLASS.PROGRAM_CLASS_BOOKING_YN
-    })
-
     this.$store.watch(() => {
       if (this.$store.getters.CONSTANTS.session_alive === true) {
         this.isLogin = true
-      } else if (this.$store.getters.CONSTANTS.session_alive === false) {
-        this.isLogin = false
+
+        let status = ''
+        STORE.getProgramClassBook(this.classId).then(result => {
+          if (result['PROGRAM_CLASS'].length > 0) {
+            _.forEach(result['PROGRAM_CLASS'], (value, index) => {
+              status = value['STATUS']
+            })
+          }
+          if (status === 'apply') {
+            this.isApply = true
+          }
+        })
       }
+    })
+
+    STORE.getProgramClass(this.$route.query.classId).then(result => {
+      this.programClass = result['PROGRAM_CLASS']
+      this.bookingAvailable = result.PROGRAM_CLASS.PROGRAM_CLASS_BOOKING_YN
     })
   },
   methods: {
+    alertAlreadyApply () {
+      this.$modal.show('dialog', {
+        title: `이미 신청하신 프로그램입니다.`,
+        text: `신청 내용 수정은 [MENU > 예약/신청내역]<br>에서 가능합니다.`,
+        buttons: [{
+          title: '확인',
+          handler: () => {
+            this.$modal.hide('dialog')
+          }
+        }]
+      })
+    },
     bookProgram () {
       if (this.isLogin) {
-        this.$router.push('/sev/booking/program/date/shop?store_id=' + process.env.FLAGSHIP_STORE_ID +
-            '&classId=' + this.$route.query.classId)
+        if (this.isApply) {
+          // 이미 신청하신 프로그램입니다.
+          this.alertAlreadyApply()
+        } else {
+          if (this.programClass['BOOKING_TYPE'] === 'basic') {
+            this.$router.push('/sev/booking/program/date/shop?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.$route.query.classId)
+          } else {
+            this.$router.push('/sev/application?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.$route.query.classId)
+          }
+        }
       } else {
         localStorage.setItem('previous_url', '/sev/booking/program/date/shop?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.$route.query.classId)
         this.$router.push({'name': 'Login'})
