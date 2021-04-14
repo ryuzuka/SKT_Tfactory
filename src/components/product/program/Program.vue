9a4bd772bbb9ab65fd1458ae7012672ef5fc451a
ryuzuka <ryuzuka@gmail.com>
2021-04-15 01:36:38 +0900

    프로그램 공통 컴포넌트 생성

---
+++ src/components/product/program/Program.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="btn-wrap sticky">
+    <a v-if="bookingType === 'basic'" @click.prevent="bookProgram" class="btn-solid">프로그램 예약</a>
+    <template v-else-if="bookingType === 'select'">
+      <a v-if="statusFlag === 'ONGOING'" href="#none" @click.prevent="bookProgram" class="btn-solid">프로그램 신청</a>
+      <button v-else-if="statusFlag === 'OVER'" class="btn-solid disabled" disabled>프로그램 신청 마감</button>
+      <button v-else-if="statusFlag === 'NOT_STARTED'" class="btn-solid disabled" disabled>신청 기간이 아닙니다.</button>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProgramBottom',
+  props: {
+    classId: {
+      type: String,
+      default: ''
+    },
+    isLogin: {
+      type: Boolean,
+      default: false
+    },
+    isApply: {
+      type: Boolean,
+      default: false
+    },
+    bookingType: {
+      type: String,
+      default: ''
+    },
+    statusFlag: {
+      type: String,
+      default: 'NOT_STARTED'
+    }
+  },
+  data () {
+    return {}
+  },
+  methods: {
+    alertAlreadyApply () {
+      this.$modal.show('dialog', {
+        title: `이미 신청하신 프로그램입니다.`,
+        text: `신청 내용 수정은 [MENU > 예약/신청내역]<br>에서 가능합니다.`,
+        buttons: [{
+          title: '확인',
+          handler: () => {
+            this.$modal.hide('dialog')
+          }
+        }]
+      })
+    },
+    bookProgram () {
+      if (this.isLogin) {
+        if (this.isApply) {
+          // 이미 신청하신 프로그램입니다.
+          this.alertAlreadyApply()
+        } else {
+          if (this.bookingType === 'basic') {
+            this.$router.push('/sev/booking/program/date/shop?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.classId)
+          } else {
+            this.$router.push('/sev/application?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.classId)
+          }
+        }
+      } else {
+        localStorage.setItem('previous_url', '/sev/booking/program/date/shop?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.classId)
+        this.$router.push({'name': 'Login'})
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
--- src/components/product/program/Program11.vue
+++ src/components/product/program/Program11.vue
@@ -1,20 +1,22 @@
 <template src="../../../assets/html/product/program/program11.html"></template>
 
 <script>
-import * as STORE from '../../../js/store.js'
 import _ from 'lodash'
+
+import * as STORE from '../../../js/store'
+import Program from './Program'
 
 export default {
   name: 'Program11',
+  components: {
+    Program
+  },
   data () {
     return {
-      programClass: null,
       classId: this.$route.query.classId,
-      storeId: '',
-      programInfo: {},
-      isLogin: false,
-      statusFlag: 'NOT_STARTED',
       bookingType: '',
+      statusFlag: '',
+      isLogin: false,
       isApply: false
     }
   },
@@ -22,61 +24,31 @@ export default {
     this.$store.watch(() => {
       if (this.$store.getters.CONSTANTS.session_alive === true) {
         this.isLogin = true
-
-        let status = ''
-        STORE.getProgramClassBook(this.classId).then(result => {
-          if (result['PROGRAM_CLASS'].length > 0) {
-            _.forEach(result['PROGRAM_CLASS'], (value, index) => {
-              status = value['STATUS']
-            })
-          }
-          if (status === 'apply') {
-            this.isApply = true
-          }
-        })
       }
     })
 
-    STORE.getProgramClass(this.$route.query.classId).then(result => {
-      this.programClass = result['PROGRAM_CLASS']
-      this.statusFlag = result['PROGRAM_CLASS']['APPLY_PROGRESS']
-      this.bookingType = result['PROGRAM_CLASS']['BOOKING_TYPE']
-    })
-  },
-  methods: {
-    alertAlreadyApply () {
-      this.$modal.show('dialog', {
-        title: `이미 신청하신 프로그램입니다.`,
-        text: `신청 내용 수정은 [MENU > 예약/신청내역]<br>에서 가능합니다.`,
-        buttons: [{
-          title: '확인',
-          handler: () => {
-            this.$modal.hide('dialog')
-          }
-        }]
+    let status = ''
+    STORE.getProgramClassBook(this.classId).then(result => {
+      if (result['PROGRAM_CLASS'].length === 0) return
+
+      _.forEach(result['PROGRAM_CLASS'], program => {
+        status = program['STATUS']
       })
-    },
-    bookProgram () {
-      if (this.isLogin) {
-        if (this.isApply) {
-          // 이미 신청하신 프로그램입니다.
-          this.alertAlreadyApply()
-        } else {
-          if (this.programClass['BOOKING_TYPE'] === 'basic') {
-            this.$router.push('/sev/booking/program/date/shop?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.$route.query.classId)
-          } else {
-            this.$router.push('/sev/application?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.$route.query.classId)
-          }
-        }
-      } else {
-        localStorage.setItem('previous_url', '/sev/booking/program/date/shop?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.$route.query.classId)
-        this.$router.push({'name': 'Login'})
+      if (status === 'apply') {
+        this.isApply = true
       }
-    }
+    })
+
+    STORE.getProgramClass(this.classId).then(result => {
+      if (!result['PROGRAM_CLASS']) return
+
+      this.bookingType = result['PROGRAM_CLASS']['BOOKING_TYPE']
+      this.statusFlag = result['PROGRAM_CLASS']['APPLY_PROGRESS']
+    })
   }
 }
 </script>
 
 <style lang="scss">
-  @import 'src/assets/css/product';
+@import 'src/assets/css/product';
 </style>
