976bba0b2998ffd2f260d218e237c2fa8ef0a2b0
ksg <ryuzuka@regularbold.co.kr>
2021-04-20 17:00:03 +0900

    사전방문예약이벤트 -> 프로그램 응모

--- src/components/product/program/Program24.vue
+++ src/components/product/program/Program24.vue
@@ -1,29 +1,50 @@
 <template src="../../../assets/html/product/program/program24.html"></template>
 
 <script>
+import _ from 'lodash'
+
+import * as STORE from '../../../js/store'
+
 export default {
   name: 'Program24',
   data () {
     return {
+      classId: this.$route.query.classId,
       bookingAvailable: false,
-      isLogin: ''
+      isLogin: false,
+      isApply: false
     }
   },
   mounted () {
     this.$store.watch(() => {
       if (this.$store.getters.CONSTANTS.session_alive === true) {
         this.isLogin = true
-      } else if (this.$store.getters.CONSTANTS.session_alive === false) {
-        this.isLogin = false
+
+        let status = ''
+        STORE.getProgramClassBook(this.classId).then(result => {
+          if (result['PROGRAM_CLASS'].length === 0) return
+
+          _.forEach(result['PROGRAM_CLASS'], program => {
+            status = program['STATUS']
+          })
+          if (status === 'apply') {
+            this.isApply = true
+          }
+        })
       }
     })
   },
   methods: {
     bookProgram () {
       if (this.isLogin) {
-        this.$router.push('/sev/booking/program/date/shop?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.$route.query.classId)
+        if (this.isApply) {
+          // 이미 신청하신 프로그램입니다.
+          this.alertAlreadyApply()
+        } else {
+          this.$router.push('/sev/application?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.classId)
+        }
       } else {
-        localStorage.setItem('previous_url', '/sev/booking/program/date/shop?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.$route.query.classId)
+        localStorage.setItem('previous_url', '/sev/application?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.classId)
         this.$router.push({'name': 'Login'})
       }
     }
