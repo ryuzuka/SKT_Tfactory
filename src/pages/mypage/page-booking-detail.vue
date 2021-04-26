bec175e9439d8616e9e9e3e6118314e9863bd9c8
ryuzuka <ryuzuka@gmail.com>
2021-04-26 12:38:13 +0900

    예약 상세 헤더 텍스트 분기처리

--- src/pages/mypage/page-booking-detail.vue
+++ src/pages/mypage/page-booking-detail.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrap">
-    <Header type="back" :title="$t('my.booking-list-detail')"></Header> <!-- 21.03.30 수정 : 예약 상세 내역 -->
-    <!-- <Header type="back" :title="$t('my.application-list-detail')"></Header> --> <!-- 21.03.30 추가 : 신청 상세 내역 -->
+    <Header v-if="applicationType === 'basic'" type="back" :title="$t('my.booking-list-detail')"></Header> <!-- 21.03.30 수정 : 예약 상세 내역 -->
+    <Header v-else-if="applicationType === 'select'" type="back" :title="$t('my.application-list-detail')"></Header> <!-- 21.03.30 추가 : 신청 상세 내역 -->
     <MyBookingDetail :booking-type="bookingType"></MyBookingDetail>
 
     <modals-container></modals-container>
@@ -21,10 +21,16 @@ export default {
   },
   data () {
     return {
-      bookingType: this.$route.params.type
+      bookingType: this.$route.params.type,
+      applicationType: 'basic'
     }
   },
-  created () {
+  mounted () {
+    this.$parent.$on('change-application-type', e => {
+      if (e.type === 'select') {
+        this.applicationType = 'select'
+      }
+    })
   }
 }
 </script>
