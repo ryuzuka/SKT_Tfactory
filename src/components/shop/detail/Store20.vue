253f90907a28e436022640042963d0c3a8803f6e
ksg <ryuzuka@regularbold.co.kr>
2021-05-10 11:00:54 +0900

    isApply 삭제

--- src/components/shop/detail/Store20.vue
+++ src/components/shop/detail/Store20.vue
@@ -1,16 +1,12 @@
 <template src="../../../assets/html/shop/detail/details20.html"></template>
 
 <script>
-import * as STORE from '../../../js/store'
-import _ from 'lodash'
-
 export default {
   name: 'Store20',
   data () {
     return {
       classId: null,
-      isLogin: false,
-      isApply: false
+      isLogin: false
     }
   },
   created () {
@@ -24,42 +20,13 @@ export default {
     this.$store.watch(() => {
       if (this.$store.getters.CONSTANTS.session_alive === true) {
         this.isLogin = true
-
-        let status = ''
-        STORE.getProgramClassBook(this.classId).then(result => {
-          if (result['PROGRAM_CLASS'].length === 0) return
-
-          _.forEach(result['PROGRAM_CLASS'], program => {
-            status = program['STATUS']
-          })
-          if (status === 'ordered') {
-            this.isApply = true
-          }
-        })
       }
     })
   },
   methods: {
-    alertAlreadyApply () {
-      this.$modal.show('dialog', {
-        title: `이미 신청하신 프로그램입니다.`,
-        text: `신청 내용 수정은 [MENU > 예약/신청내역]<br>에서 가능합니다.`,
-        buttons: [{
-          title: '확인',
-          handler: () => {
-            this.$modal.hide('dialog')
-          }
-        }]
-      })
-    },
     reserve () {
       if (this.isLogin) {
-        if (this.isApply) {
-          // 이미 신청하신 프로그램입니다.
-          this.alertAlreadyApply()
-        } else {
-          this.$router.push('/sev/booking/program/date/shop?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.classId)
-        }
+        this.$router.push('/sev/booking/program/date/shop?store_id=' + process.env.FLAGSHIP_STORE_ID + '&classId=' + this.classId)
       } else {
         localStorage.setItem('previous_url', this.$route.path + '?classId=' + this.classId)
         this.$router.push({'name': 'Login'})
