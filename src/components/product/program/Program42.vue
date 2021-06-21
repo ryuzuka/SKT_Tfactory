e3a7cfea12a521a64122c310a02a8bb7010d9b7c
bstones <bstones01>
2021-06-21 10:17:14 +0900

    program40,41,42,43,44 생성

---
+++ src/components/product/program/Program42.vue
@@ -0,0 +1,26 @@
+<template src="../../../assets/html/product/program/program42.html"></template>
+
+<script>
+import Program from './Program'
+
+export default {
+  name: 'Program37',
+  components: {
+    Program
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+  @import 'src/assets/css/product';
+</style>
---
+++ src/components/product/program/Program44.vue
@@ -0,0 +1,37 @@
+<template src="../../../assets/html/product/program/program44.html"></template>
+
+<script>
+import _ from 'lodash'
+import * as STORE from '../../../js/store'
+
+export default {
+  name: 'Program39',
+  components: {},
+  data () {
+    return {
+      classIdList: [56, 58, 59, 60],
+      programClass: [{}, {}, {}, {}]
+    }
+  },
+  mounted () {
+    this.$store.watch(() => {
+      if (this.$store.getters.CONSTANTS.session_alive === true) {
+        this.isLogin = true
+      }
+    })
+
+    _.forEach(this.classIdList, (classId, index) => {
+      STORE.getProgramClass(classId).then(result => {
+        this.programClass[index] = result['PROGRAM_CLASS']
+        this.$forceUpdate()
+      })
+    })
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+  @import 'src/assets/css/product';
+</style>
