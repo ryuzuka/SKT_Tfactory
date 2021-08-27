c035eeda57386093656d2d09d0b36c9ef2427871
bstones <bstones01>
2021-08-27 14:52:01 +0900

    program48완료

---
+++ src/components/product/program/Program48.vue
@@ -0,0 +1,48 @@
+<template src="../../../assets/html/product/program/program48.html"></template>
+
+<script>
+import KakaoShare from '../../common/KakaoShare'
+import ProgramLineup from './components/ProgramLineup'
+import * as NATIVE from '../../../js/native'
+import * as STORE from '../../../js/store'
+
+export default {
+  name: 'Program48',
+  components: {
+    KakaoShare,
+    ProgramLineup
+  },
+  data () {
+    return {}
+  },
+  created () {
+    STORE.getProgramClass(this.classId).then(result => {
+      if (!result['PROGRAM_CLASS']) return
+
+      console.log(result['PROGRAM_CLASS'])
+    })
+  },
+  mounted () {
+    this.$store.watch(() => {
+      if (this.$store.getters.CONSTANTS.session_alive === true) {
+        this.isLogin = true
+      }
+    })
+  },
+  methods: {
+    clickButton () {
+      const redirectURL = 'https://www.instagram.com/tfactory_sampler/'
+      const mobileOS = this.$cookies.get('platform')
+      if (mobileOS === 'A' || mobileOS === 'I') {
+        NATIVE.sysBrowserOpen(mobileOS, redirectURL)
+      } else {
+        window.open(redirectURL, '_blank')
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import 'src/assets/css/product';
+</style>
--- src/pages/product/page-experience.vue
+++ src/pages/product/page-experience.vue
@@ -48,6 +48,8 @@
     <program44 v-else-if="programId === 'program44'"></program44>
     <program45 v-else-if="programId === 'program45'"></program45>
     <program46 v-else-if="programId === 'program46'"></program46>
+    <!-- <program47 v-else-if="programId === 'program47'"></program47> -->
+    <program48 v-else-if="programId === 'program48'"></program48>
     <Experience v-else></Experience>
 
     <modals-container></modals-container>
@@ -105,6 +107,8 @@ import Program43 from '../../components/product/program/Program43'
 import Program44 from '../../components/product/program/Program44'
 import Program45 from '../../components/product/program/Program45'
 import Program46 from '../../components/product/program/Program46'
+// import Program47 from '../../components/product/program/Program47'
+import Program48 from '../../components/product/program/Program48'
 
 export default {
   name: 'page-product',
@@ -156,7 +160,9 @@ export default {
     Program43,
     Program44,
     Program45,
-    Program46
+    Program46,
+    // Program47,
+    Program48
   },
   data () {
     return {
