01b9e3b67059b8618a45cb98fb05a3fb348f0c5a
bstones <bstones01>
2021-10-07 14:46:49 +0900

    program46 수정, program56,57 생성

---
+++ src/components/product/program/Program56.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="contents" ref="contents">
+    <!-- slide -->
+    <div class="slider-prd">
+      <img src="../../../assets/images/program/class56-slide1.jpg" alt="">
+    </div>
+    <!-- //slide -->
+    <!-- details -->
+    <div class="details sticky">
+      <div class="details-header">
+        <!-- <kakao-share
+          title="인스타용 느낌있는 사진 찍기"
+          description="셀린&루나와 함께하는 입문자용 사진 촬영/편집 원데이 클래스"
+        ></kakao-share> -->
+
+        <em class="flag-solid7">CONCERT</em>
+        <h2 class="details-tit">
+          ADOY를 영화처럼<br>
+          담아줄 팬분을 찾습니다.
+        </h2>
+        <div class="tags">
+          <span class="flag-basic">인디밴드</span>
+          <span class="flag-basic">아이폰13</span>
+          <span class="flag-basic">덕콘</span>
+        </div>
+        <p class="desc type2">
+          찐팬이 보는 ADOY의 공연은 어떨까?<br>
+          &#91;덕콘 x 아이폰13 | ADOY&#93;를 <br>
+          영화처럼 담아주실 팬분을 찾습니다.
+        </p>
+      </div>
+      <div class="details-inner">
+        <h3 class="subtit">모집 안내</h3>
+        <div class="thumb"><img src="../../../assets/images/program/class56-conts.jpg" alt=""></div>
+        <p>
+          팬 분들의 시점을 대표해주실 팬 세분을 찾습니다.<br>
+          이번 덕콘은 비대면 온라인으로 진행되며, <br>
+          T Factory 인스타그램을 통해 아이폰13으로 찍은 공연 사진, 영상이 공유될 예정입니다.
+        </p>
+        <p style="margin-top:22px">
+          애정이 듬뿍 담긴 팬의 시선으로<br>
+          아이폰13을 활용해 ADOY를 영화처럼 담아주실 팬분을 찾습니다.<br>
+          성덕이 될 수 있는 기회! ADOY의 매력을 널리 알릴 자신이 있다면, 바로 지원해주세요.
+        </p>
+        <p style="margin-top:22px">
+          선정된 세분은 10월 15일 ADOY 덕콘 당일,<br>
+          아이폰13으로 ADOY의 멋진 모습을 담아주시면 됩니다.<br>
+          당일 아이폰13 촬영 사전교육 및 리허설, 본 공연에 참여되며, 아래와 같이 혜택을 드립니다.
+        </p>
+        <p style="margin-top:22px">※ 본 행사는 코로나19 방역 수칙을 준수하여 진행됩니다.</p>
+        <h3 class="subtit">혜택 안내</h3>
+        <ol class="step-list" start="margin-top:20px">
+          <li>현장에서 ADOY의 공연을 실시간으로 관람하며, <br>
+            애정을 담은 사진과 영상을 직접 촬영할 수 있습니다.
+          </li>
+          <li>ADOY 굿즈(티셔츠+싸인CD) 및
+            T Factory 굿즈 세트를 드립니다.
+          </li>
+          <li>ADOY와 단독 사진 촬영의 기회를 드립니다.</li>
+        </ol>
+        <h3 class="subtit">응모 방법</h3>
+        <p style="margin-top:12px">
+          하단의 지원서 응모버튼을 눌러,
+          내가 잘 할 수 있는 이유와
+          직접 찍은 사진과 영상 파일 첨부하여
+          skt.tfactory@gmail.com으로 보내주세요.
+        </p>
+        <ul class="texts-info" style="margin-top:18px">
+          <li>* ADOY를 찍은 사진/영상을 보내주시면 가산점이 있습니다.</li>
+        </ul>
+        <h3 class="subtit">일정 및 장소</h3>
+        <ul class="texts-list">
+          <li>응모기간 : 10월 8일(금)~10월 11일(월)</li>
+          <li>당첨자 발표 : 10월 12일(화) 메일로 회신</li>
+          <li>행사진행 : 10월 15일(금) 오후 3시 ~ 9시</li>
+          <li>장소 : T Factory 2층, 0 Stage</li>
+        </ul>
+        <div class="notice">
+        <h4 class="subtit">{{ $t('prd.notice')}}</h4>
+        <ul class="texts-info">
+          <li>* 선정되신 분은 당첨자 발표일에 보내주신 메일 주소로 회신드리며, 당일 확인 응답이 없으시면 선정이 취소될 수 있습니다.</li>
+          <li>* 선정되신 분은 공연 당일 진행되는 사전교육(오후3시)에 필수로 참석하셔야합니다.</li>
+          <li>* 오후 4시에 진행되는 리허설부터 본 공연까지 촬영이 진행됩니다.(저녁 제공)</li>
+        </ul>
+      </div>
+      </div>
+    </div>
+    <!-- //details -->
+  </div>
+</template>
+
+<script>
+import KakaoShare from '../../common/KakaoShare'
+import ProgramButton from './components/ProgramButton'
+
+export default {
+  name: 'Program56',
+  components: {
+    KakaoShare,
+    ProgramButton
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+  @import 'src/assets/css/product';
+  .texts-list li:nth-child(2):before {
+    content: '-' !important;
+    margin-left: -9px;
+  }
+</style>
---
+++ src/components/product/program/Program57.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="contents" ref="contents">
+    <!-- slide -->
+    <div class="slider-prd">
+      <img src="../../../assets/images/program/class57-slide1.jpg" alt="">
+    </div>
+    <!-- //slide -->
+    <!-- details -->
+    <div class="details sticky">
+      <div class="details-header">
+        <!-- <kakao-share
+          title="인스타용 느낌있는 사진 찍기"
+          description="셀린&루나와 함께하는 입문자용 사진 촬영/편집 원데이 클래스"
+        ></kakao-share> -->
+
+        <em class="flag-solid7">Exhibition</em>
+        <h2 class="details-tit">
+          하시시박의 일주일<br>
+          Ordinary week,<br>
+          extraordinary iPhone13
+        </h2>
+        <div class="tags">
+          <span class="flag-basic">특별사진전</span>
+          <span class="flag-basic">아이폰13</span>
+        </div>
+        <p class="desc type2">
+          아이폰13으로 담은 하시시박의 일주일을 들여다보세요.<br>
+          &#60;하시시박의 일주일 : Ordinary week, extraordinary iPhone13&#62;은 아이폰13 출시를 기념하여 <br>
+          SK텔레콤 플래그십 스토어 T Factory에서 준비한 
+          특별 사진 전시회입니다. 
+        </p>
+      </div>
+      <div class="details-inner">
+        <h3 class="subtit">전시 소개</h3>
+        <div class="thumb"><img src="../../../assets/images/program/class57-conts.jpg" alt=""></div>
+        <p>
+          지극히 평범한 일상도 아이폰13과 함께하면 영화의 한 장면으로 다시 태어난다. <br>
+          이 것은 일상에서 찍은 사진들을 모아 자연스럽게 주제나 스토리를 만드는 포토그래퍼 하시시박의 평소 작업 스타일과도 많이 닮아 있다. <br>
+          그녀가 아이폰13을 들자 육아와 살림의 평범한 단면이 작품이 되어 돌아왔다. <br>
+          어쩌면 전문 사진 작가도 일상에서 아이폰을 사진기로 가장 많이 사용하고 있지 않을까. <br>
+          그렇기 때문에 그녀의 사진들은 또다시 누구라도 아이폰13과 함께라면 포토그래퍼가 될 수 있음을 말해준다.
+        </p>
+        <p style="margin-top:22px">
+          아이폰13의 여러가지 사진 기능을 다루다 보면 ‘빛’을 다루는 세밀한 작업부터 피사체의 흔들림 보정까지 넓은 스펙트럼을 경험할 수 있다.<br>
+          이제 저마다의 하루를 작품으로 담아보자. 
+        </p>
+        <h3 class="subtit">작가 소개</h3>
+        <p>
+          하시시박&#40;Hasisi Park&#41;은 필름 카메라 특유의 아날로그적 감성과 예리한 화법으로 일상의 다양한 표정을 찍는 사진가이다.<br>
+          &#60;바이스&#62; 매거진 영국판에서 데뷔 후 서울을 기반으로 활동하고 있다. 브랜드와의 협업, 매체 및 화보 등 커머셜 촬영과 개인 작업을 넘나 들며 자신만의 세계를 독보적으로 쌓고 있다.
+        </p>
+        <h3 class="subtit">한정판 포토스티커 이벤트</h3>
+        <p>
+          &#60;하시시박의 일주일&#62; A WEEK 포토존에서 멋진 사진을 남기시고,<br>
+          아래 해시태그와 함께 인스타그램에 올려주세요.<br>
+          <span style="color:#3617ce;">#하시시박의일주일 #아이폰13사진전 #T팩토리</span><br>
+          업로드된 게시물을 크루에게 보여주시면, <br>
+          현장에서 한정판 하시시박 포토 스티커를 드립니다.
+        </p>
+        <ul class="texts-info" style="margin-top:18px">
+          <li>* 본 이벤트는 상품 재고 사정에 따라 조기 마감될 수 있습니다.</li>
+          <li>* 본 이벤트 참여와 관련하여 자세한 내용은 현장 안내를 참조해주세요. </li>
+        </ul>
+        <h3 class="subtit">일정 및 장소</h3>
+        <ul class="texts-list">
+          <li>일정 : 10월 8일(금) ~ 11월 중순 </li>
+          <li>장소 : T Factory 2층, 모바일 Acc 존</li>
+        </ul>
+        <ul class="texts-info" style="margin-top:18px">
+          <li>* 본 전시는 사전 예약 없이 관람이 가능한 아이폰13 출시기념 무료 전시회입니다.</li>
+          <li>* 본 사진전은 코로나19 방역수칙을 준수하여 운영됩니다. </li>
+        </ul>
+      </div>
+    </div>
+    <!-- //details -->
+  </div>
+</template>
+
+<script>
+import KakaoShare from '../../common/KakaoShare'
+import ProgramButton from './components/ProgramButton'
+
+export default {
+  name: 'Program57',
+  components: {
+    KakaoShare,
+    ProgramButton
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+  @import 'src/assets/css/product';
+  .texts-list li:nth-child(2):before {
+    content: '-' !important;
+    margin-left: -9px;
+  }
+</style>
