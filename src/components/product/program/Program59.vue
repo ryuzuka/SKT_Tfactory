f090af327ec48ffea38d14778ae5aa2ad11356f8
ryuzuka1222 <fprbffjqhfem01!>
2021-10-18 12:23:41 +0900

    프로그램 43 58 59 - 덕콘 보러가기 (PROGRAM_CLASS)

--- src/components/product/program/Program59.vue
+++ src/components/product/program/Program59.vue
@@ -75,6 +75,7 @@
     <div class="btn-wrap sticky">
       <a href="#" @click.prevent="clickButton" class="btn-solid">덕콘 보러가기</a>
     </div>
+    <program-button v-show="false"></program-button>
     <!-- // button set -->
   </div>
 </template>
