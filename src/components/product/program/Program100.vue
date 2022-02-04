11abb8cd7392ed90ed0c939b5eef11fd884e06a2
joonhong5027 <joonhong5027@gmail.com>
2022-02-04 14:02:07 +0900

    Program 100 신규생성, 크루투어 하단 방역패스 추가

--- src/components/product/program/Program100.vue
+++ src/components/product/program/Program100.vue
@@ -2,50 +2,106 @@
   <div class="contents bottom-sticky" ref="contents">
     <!-- slide -->
     <div class="slider-prd">
-      <img src="../../../assets/images/program/class97-slide1.jpg" alt="<NEXT 트레이서>">
+      <img src="../../../assets/images/program/class100-slide1.jpg" alt="T Factory X FLO 덕후콘 | 2022.2 데자부 그룹">
     </div>
     <!-- //slide -->
     <!-- details -->
     <div class="details sticky">
       <div class="details-header">
         <kakao-share
-          title="<NEXT 트레이서>"
-          description="조세5국 신입 사원이 되어 숨겨진 돈을 찾아라"
+          title="2월의 덕콘 | 데자부 그룹"
+          description="찐덕들만 초청하여 진행하는 덕콘. 이번 기출영역은 ‘데자부 그룹'입니다."
         ></kakao-share>
 
-        <em class="flag-solid7">EXHIBITION</em>
+        <em class="flag-solid7">CONCERT</em>
         <h2 class="details-tit">
-          &#60;NEXT 트레이서&#62;
-          </h2>
+          2월의 덕콘<i class="pipe"></i>데자부 그룹
+        </h2>
         <div class="tags">
-          <span class="flag-basic">전시</span>
-          <span class="flag-basic">체험</span>
-          <span class="flag-basic">이벤트</span>
+          <span class="flag-basic">공연관람형</span>
+          <span class="flag-basic">소규모콘서트</span>
+          <span class="flag-basic">아티스트</span>
         </div>
-        <p class="desc type2">조세5국 신입 사원이 되어 숨겨진 돈을 찾아라</p>
+        <p class="desc type2">
+          <em>[T Factory X FLO 덕콘]</em>
+          찐덕들만 초청하여 진행하는 덕콘.<br>
+          이번 기출영역은 '데자부 그룹'입니다.
+        </p>
       </div>
-      <div class="details-inner">
-        <h3 class="subtit">전시 소개</h3>
+      <div class="details-inner" style="padding-bottom:40px;">
+        <h3 class="subtit">2월의 덕콘!</h3>
         <p>
-          2022년 wavve 오리지널 콘텐츠 &#60;트레이서&#62;<br>
-          누군가에겐 판검사보다 무서운 곳, 국세청. 일명  ‘쓰레기 하치장’이라 불리는 조세5국에 굴러온 독한 놈의 물불 안 가리는 활약을 그린 통쾌한 추적 활극 &#60;트레이서&#62;. 다소 무거울 수 있는 주제를 유쾌하게 그려내며 시즌 1의 마지막까지 시청률 고공 행진을 거듭했습니다. 종영이 아쉬운 분들을 위해 T Factory와 wavve가 특별한 방 탈출 이벤트를 준비했습니다.<br><br>
-          이제 여러분이 ‘NEXT 트레이서’입니다. 트레이서의 주인공처럼 숨겨진 돈에 대한 단서를 찾아 돈벼락을 맞는 통쾌함을 경험해 보세요!
+          2월에도 다양한 장르의 특별한 아티스트들과<br>
+          함께합니다.<br>
+          아티스트가 낸 덕력고사 풀고<br>
+          콘서트 초청의 기회를 잡으세요!
         </p>
-        <div class="btn-wrap" style="margin: 24px 0 16px;">
-          <button class="btn-line big" @click="clickButton('https://www.instagram.com/tfactory_sampler/')">
-            공식 인스타그램 방문하기
-          </button>
-        </div>
-        <h3 class="subtit" style="margin: 48px 0 9px;">일정 및 장소</h3>
-        <p style="margin-top: 9px;">
-          2022년 2월 5일(토)~4월 중<br>
-          T Factory 홍대
+        <ul class="desc-list" style="margin-top:12px;">
+          <li><em style="font-weight:500;">2월의 덕콘 라인업</em></li>
+          <li style="margin-top:4px;"><span class="day" style="width:36px;">2.11</span> 안녕하신가영</li>
+          <li><span class="day" style="width:36px;">2.25</span> 데자부그룹</li>
+          <ul class="texts-list" style="margin-top:0; margin-left:44px;">
+          <li>Khundi Panda (쿤디판다)</li>
+          <li>으네 (UNE)</li>
+          <li>Son Simba (손 심바)</li>
+          <li>Viann (비앙)</li>
+          <li>최엘비 (CHOILB)</li>
+        </ul>
+        </ul>
+      </div>
+      <div class="details-inner" style="padding: 30px 20px 32px;">
+        <h3 class="subtit">T Factory x FLO 덕콘(THECON)</h3>
+        <div class="thumb"><img src="../../../assets/images/program/class86-conts1.jpg" alt="T Factory x FLO 입덕후콘서트"></div>
+        <p>
+          T Factory와 FLO가 함께 만드는 팬 초청 소규모 콘서트를 소개합니다.<br>
+          콘서트가 사라진 요즘, 홍대 인디문화 부흥과 일상의 작은 위로를 전하고자 T Factory에서 매월 다양한 아티스트를 선정하여 무료 콘서트를 진행합니다.<br>
+          아티스트가 직접 낸 덕력고사를 통과한 팬들 중 추첨을 통해 콘서트 초청의 기회를 드립니다.
         </p>
+        <ul style="margin-top: 22px;">
+          <li>※ 본 콘서트는 코로나19 방역 수칙을 준수하여 진행됩니다.</li>
+          <li>※ 사회적 거리두기 단계에 따라 비대면으로 전환될 수 있습니다.</li>
+        </ul>
+        <h3 class="subtit" style="margin-top: 40px;">데자부 그룹 소개</h3>
+        <p>
+           데자부그룹 : 데자부그룹은 2017년 설립된 레이블로 설립 초반은 비와이 1인 기획사로 운영되었다가 2019년 비앙, 손심바, 쿤디판다와 계약하면서 힙합씬의 메인 레이블로 자리잡았다.<br>
+           2021년에는 최엘비, 으네 (UNE)와 계약하면서 힙합뿐만이 아닌 다양한 음악을 하는 아티스트를 보유한 레이블이 되었다. 최엘비와 으네와 계약하면서 딩고와의 단체 예능/음반 프로젝트 "힙합청년부 데자부"를 진행하면서 대중들에게도 많은 관심을 받게되었다.
+        </p>
+        <div class="thumb"><img src="../../../assets/images/program/class100-conts1.jpg" alt=""></div>
+        <p>
+           으네 : 으네는 전자음악, 팝 계열의 음악을 하는 싱어송라이터이다. 2021년 비와이가 대표로 있는 '데자부그룹'에 새롭게 합류하면서 많은 대중, 장르팬들의 관심을 받았습니다. 합류 이후에 딩고와의 협업 예능/음반 프로젝트, 'The very first episode' 더블싱글 발매, 코지팝 프로젝트 등 다양한 활동을 이어가고 있다.
+        </p>
+        <div class="thumb"><img src="../../../assets/images/program/class100-conts2.jpg" alt=""></div>
+        <p>
+           쿤디판다 : 쿤디판다는 날카로운 하이톤으로 정교한 랩을 내뱉는다. 랩 뿐만 아니라 가사, 그리고 앨범을 만드는 능력 또한 출중해서 2018, 2021년 두번에 걸쳐서 한국대중음악상 최우수 랩/힙합 음반상을 수상하였다. 또한 2020년에는 쇼미더머니 9에 출연하여 세미파이널까지 진출하며 대중들에게도 많은 관심을 받게 되었다.
+        </p>
+        <div class="thumb"><img src="../../../assets/images/program/class100-conts3.jpg" alt=""></div>
+        <p>
+           손심바 : 붐뱁을 지향하는 래퍼이며, 14, 15년에 나온 믹스테입과 앨범들이 천천히 주목을 받고 당시 섹시스트릿의 샤라웃을 받으며 어느정도 컨셔스 래퍼로서 리스너들에게 알려졌다.<br>
+           큰 기교나 독특한 플로우 없이 본인의 유려한 톤과, 치밀한 라임으로 랩을 이어나간다. 손심바는 가사를 진솔하게 잘 쓰는 래퍼이고 특유의 라임과 톤, 뛰어난 딜리버리를 통해 가사에 담긴 의미를 청자에게 잘 전달한다는 것이 가장 큰 무기인 래퍼이다.
+        </p>
+        <div class="thumb"><img src="../../../assets/images/program/class100-conts4.jpg" alt=""></div>
+        <p>
+           비앙 : 2017년 11월, 쿤디판다의 합작 앨범인 &#60;재건축&#62;을 발매했고, 2018년 한국대중음악상 최우수 힙합앨범 부문에서 &#60;재건축&#62;으로 쿤디판다와 함께 수상한다. 베이스 뮤직과 힙합을 기반으로 한 자신의 음악 세계를 드러내며 수민, 우탄과 협업하며 씬의 주목을 받았다.<br>
+           또한 비앙은 힙합 음악에 국한되지 않고, 전자 음악, 팝 등 여러 장르의 음악도 제작한다. 2020년에는 비와이 X 슬기의 폭스바겐 광고 음원을 포함해서 다양한 광고 음악도 프로듀싱하였다
+        </p>
+        <div class="thumb"><img src="../../../assets/images/program/class100-conts5.jpg" alt=""></div>
+        <p>
+           최엘비는 힙합 레이블 데자부그룹 소속래퍼입니다. 누구보다 솔직하고 단단한 스토리텔링으로 앨범 단위의 활동에서 더욱 빛을 발하고 있는 아티스트입니다. 정규 1집 [오리엔테이션]은 한국대중음악상 최우수 랩&힙합 음반 수상후보에 노미네이트 되었습니다. 데자부그룹에 합류하고 발표한 정규3집 [독립음악]은 프로듀싱과 구성이 탄탄함과 동시에 많은 이들의 공감을 자아내는 솔직한 가사로 “올해의 음반”후보로 거론될 정도로 많은 관심을 받고 있습니다.
+        </p>
+        <h3 class="subtit" style="margin-top: 40px;">{{ $t('prd.schedule-place')}}</h3>
+        <ul class="texts-list">
+          <li>일정 : 2월 25일(금) 오후 7시</li>
+          <li>응모기간 : 2월 14일(월) ~ 2월 20일(일)</li>
+          <li>당첨자 발표 : 2월 21(월)</li>
+          <li>장소 : T Factory 2층, 0 Stage</li>
+        </ul>
         <div class="notice">
-          <h4 class="subtit">유의사항</h4>
+          <h4 class="subtit">{{ $t('prd.notice')}}</h4>
           <ul class="texts-info">
-            <li>* 이 전시는 T Factory 홍대점을 방문하여 스마트폰으로 체크인할 경우에만 참여하실 수 있습니다. (체크인 가능 연령: 만 14세 이상)</li>
-            <li style="margin-top:12px">* 이 전시의 자세한 사항은 업체 사정에 따라 안내 없이 중단되거나 변경될 수 있습니다.</li>
+            <li>* 본 공연은 덕력고사를 완료하신 분들 중 추첨을 통해 선정되며, 선정자는 개별 연락드립니다.</li>
+            <li style="margin-top: 4px;">* 당첨자 중 무단 노쇼하시는 경우, 추후 콘서트 당첨에서 제외됩니다.</li>
+            <li style="margin-top: 4px;">* 정부의 방역지침 변경이 있을 시 공연의 주요내용도 변경이 있을 수 있습니다.</li>
+            <li style="margin-top: 4px;">* 덕콘은 14세 미만의 고객도 보호자 동반 하에 당첨 및 참여가 가능하나, 보호자도 함께 응모에 당첨되어야 하는점 유의부탁드립니다.</li>
           </ul>
         </div>
         <div class="notice" style="margin-top:24px;">
@@ -57,7 +113,7 @@
             <li>4. 확진 후 완치자 (격리해제 확인서)</li>
             <li>5. 백신접종 예외자 (보건소 발급소견서)</li>
           </ul>
-          <p class="texts-notice" style="margin-top:20px;">본 전시는 방역수칙 준수를 위해<br>방역패스 인증자에 한해 참여가 가능합니다.</p>
+          <p class="texts-notice" style="margin-top:20px;">본 클래스/투어/덕콘은 방역수칙 준수를 위해<br>방역패스 인증자에 한해 참여가 가능합니다.</p>
         </div>
       </div>
     </div>
@@ -75,7 +131,7 @@ import ProgramButton from './components/ProgramButton'
 import * as NATIVE from '../../../js/native'
 
 export default {
-  name: 'Program84',
+  name: 'Program99',
   components: {
     KakaoShare,
     ProgramButton
@@ -89,7 +145,8 @@ export default {
   mounted () {
   },
   methods: {
-    clickButton (redirectURL) {
+    clickButton () {
+      const redirectURL = 'https://www.instagram.com/tfactory_sampler/'
       const mobileOS = this.$cookies.get('platform')
       if (mobileOS === 'A' || mobileOS === 'I') {
         NATIVE.sysBrowserOpen(mobileOS, redirectURL)
@@ -109,7 +166,8 @@ export default {
     content: '-' !important;
     margin-left: -9px;
   }
-  .step-list>li:first-child::before {
-      margin: 3px 6px 0 -22px;
+  .texts-info li:nth-of-type(n+2) {
+      margin-top: 0px;
   }
+  .experience .details-inner .thumb:nth-of-type(n+2) {margin-top: 24px;}
 </style>
