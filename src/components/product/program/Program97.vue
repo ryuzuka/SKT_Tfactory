e13439b799f5bcb2a5b682900ce351daeb8082b1
ryuzuka <ryuzuka1222>
2022-02-11 14:46:15 +0900

    program97 예약버튼 삭제

--- src/components/product/program/Program97.vue
+++ src/components/product/program/Program97.vue
@@ -64,7 +64,7 @@
     <!-- //details -->
 
     <!-- button set -->
-    <program-button></program-button>
+		<!--<program-button></program-button>-->
     <!-- // button set -->
   </div>
 </template>
