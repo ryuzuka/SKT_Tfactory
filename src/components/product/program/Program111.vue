35ce62c015753b603e40e58a6e43fdac7a6e652f
ryuzuka <ryuzuka1222>
2022-04-11 11:10:28 +0900

    TUAF, TUMF 예약버튼 삭제

--- src/components/product/program/Program111.vue
+++ src/components/product/program/Program111.vue
@@ -263,7 +263,7 @@
     <!-- //details -->
 
     <!-- button set -->
-    <program-button></program-button>
+<!--    <program-button></program-button>-->
     <!-- // button set -->
   </div>
 </template>
--- src/components/product/program/Program112.vue
+++ src/components/product/program/Program112.vue
@@ -251,7 +251,7 @@
     <!-- //details -->
 
     <!-- button set -->
-    <program-button></program-button>
+<!--    <program-button></program-button>-->
     <!-- // button set -->
   </div>
 </template>
