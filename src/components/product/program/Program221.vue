aea64be48ee244a508314c8022c453a8ad6dd1ad
ryuzuka <ryuzuka1222>
2022-08-24 14:55:16 +0900

    program221 - 요일 수정

--- src/components/product/program/Program221.vue
+++ src/components/product/program/Program221.vue
@@ -36,7 +36,7 @@
 					</dd>
 					<dd>
 						<span class="sub_title" style="width: 100px;">응모기간</span>
-						<span class="desc">8월 24일 (수) ~ 8월 25일 (금)</span>
+						<span class="desc">8월 24일 (수) ~ 8월 25일 (목)</span>
 					</dd>
 					<dd>
 						<span class="sub_title" style="width: 100px;">당첨자발표</span>
